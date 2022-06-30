39d47799321d3d3853874f8f1b80349560604e81
Xuri Li <xuri.li@cloudkitchens.com>
2022-06-30 17:51:23 +0800

    add no default server header config to fasthttp engine

--- server/engine.go
+++ server/engine.go
@@ -1,10 +1,11 @@
 package server
 
 import (
-	"github.com/valyala/fasthttp"
-	"github.com/valyala/fasthttp/fasthttpadaptor"
 	"net"
 	"net/http"
+
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
 )
 
 type Engine func(listener net.Listener, server http.Handler) error
@@ -14,5 +15,9 @@ func NetEngine(listener net.Listener, server http.Handler) error {
 }
 
 func FastHTTPEngine(listener net.Listener, server http.Handler) error {
-	return fasthttp.Serve(listener, fasthttpadaptor.NewFastHTTPHandler(server))
+	fastHTTPServer := fasthttp.Server{
+		Handler:               fasthttpadaptor.NewFastHTTPHandler(server),
+		NoDefaultServerHeader: true,
+	}
+	return fastHTTPServer.Serve(listener)
 }
